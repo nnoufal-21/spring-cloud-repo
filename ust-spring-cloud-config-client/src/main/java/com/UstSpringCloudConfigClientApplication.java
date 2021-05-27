ac6de7d4302711133ab0cfabfb11c16092ce7360
nnoufal-21 <nnoufal21@gmail.com>
2021-05-27 13:09:36 +0530

    ust-spring-cloud-config-client

---
+++ ust-spring-cloud-config-client/src/main/java/com/UstSpringCloudConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UstSpringCloudConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UstSpringCloudConfigClientApplication.class, args);
+	}
+
+}
